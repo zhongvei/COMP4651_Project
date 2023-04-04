7a1c6863fae46acbca3bfee2525ac83ee74013a0
zvei <zhongvei@gmail.com>
2023-04-04 20:40:30 +0800

    added navbar

---
+++ client/pages/rent/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Rent = () => {
+    return (
+    <h1 className="text-3xl font-bold underline text-red-700">
+        Hello world!
+      </h1>
+    );
+};
+
+export default Rent;
