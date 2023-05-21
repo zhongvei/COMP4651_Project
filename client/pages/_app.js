dd30527916876717a446bbaff45b564c3f6a251b
zvei <zhongvei@gmail.com>
2023-05-21 20:18:06 +0800

    change chain name

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -39,7 +39,7 @@ ChartJS.register(
 // This is the chain your dApp will work on.
 // Change this to the chain your app is built for.
 // You can also import additional chains from `@thirdweb-dev/chains` and pass them directly.
-const activeChain = "localhost";
+const activeChain = "170";
 
 function MyApp({ Component, pageProps }) {
 	const router = useRouter();
